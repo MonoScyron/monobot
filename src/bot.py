3f4d675b8d7a18c21278704b0b58aaaebca326b4
MonoScyron <110511317+MonoScyron@users.noreply.github.com>
2025-08-14 05:35:45 -0400

    Fix issues with first use servers

--- src/bot.py
+++ src/bot.py
@@ -518,11 +518,13 @@ async def react_role_message(ctx: commands.Context, *, msg=''):
                         f'{data_rr[guild_id]["message"]["link"]}', mention_author=False)
         return
 
-    data_rr_guild = data['react roles'][guild_id]
     if guild_id not in data['react roles']:
-        data_rr_guild = {
+        data['react roles'][guild_id] = {
+            'message': {},
             'roles': {}
         }
+    else:
+        data_rr_guild = data['react roles'][guild_id]
 
     msg = await ctx.send(__create_rr_msg(data_rr_guild['roles']))
     log.info(f'new rr message created: {msg.jump_url} in {msg.channel.name} ({msg.channel.id})')
